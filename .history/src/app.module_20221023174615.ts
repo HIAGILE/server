384b3588e28bc48f971135c6c57c0d1832700a7a
thewoowon <thewoowon@naver.com>
2022-10-23 17:57:44 +0900

    feat : app.module.ts setting

--- .history/src/app.module_20221023174615.ts
+++ .history/src/app.module_20221023174615.ts
@@ -9,16 +9,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CommonModule } from './common/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-    
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath:process.env.NODE_ENV === 'developer' ? '.env.developer' : '.env.test',
-      ignoreEnvFile:process.env.NODE_ENV === 'production',
-    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver, // 아폴로 드라이버 옵션
       autoSchemaFile: true, // Schema 파일 생성 여부 옵션
