f711eec64699fe7ad0aedffff0b7a630d43db56c
thewoowon <thewoowon@naver.com>
2022-10-23 17:01:50 +0900

    feat : add all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProjectModule } from './project/project.module';
+import { MemberModule } from './member/member.module';
+import { SprintModule } from './sprint/sprint.module';
+import { TodolistModule } from './todolist/todolist.module';
+import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver, // 아폴로 드라이버 옵션
+      autoSchemaFile: true, // Schema 파일 생성 여부 옵션
+    }),
+  
+    , UserModule, ProjectModule, MemberModule, SprintModule, TodolistModule, AuthModule, CommonModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
