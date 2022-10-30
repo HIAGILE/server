0d4d64af121d467c8b8781a7a524c31880047c02
thewoowon <thewoowon@naver.com>
2022-10-30 09:43:52 +0900

    feat : edit service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
 
@@ -42,6 +43,30 @@ export class UserService{
             ok: false,
             error: 'User Not Found',
           };
+        }
+      }
+
+      async login({email,password}):Promise<LoginOutput>{
+        try{
+
+        }
+        catch(e){
+          const user = await this.users.findOne({
+            select:{
+              email:true,
+              password:true,
+              name:true,
+            },
+            where:{
+              email:email
+            }
+          })
+          if (!user){
+            return {
+              ok:false,
+              error:'계정이 존재하지 않아요. 이메일을 확인해주세요.'
+            }
+          }
         }
       }
 }
